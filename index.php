008533a4d4e5b06082957f6635f41c9df479b79a
jim <chuang.jim@baiyitech.com.tw>
2020-05-12 18:15:48 +0800

    add

--- index.php
+++ index.php
@@ -46,6 +46,16 @@
             height: 50%;
         }
     </style>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166265389-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-166265389-1');
+</script>
+
 </head>
 
 <body>
